ea3af9f94b2010b7ad6f63832d198dec5e4aa2ff
Dorstol <olehsofronov@gmail.com>
2024-08-23 16:40:19 +0300

    first commit

---
+++ src/core/models/access_token.py
@@ -0,0 +1,24 @@
+from typing import TYPE_CHECKING
+
+from fastapi_users_db_sqlalchemy.access_token import (
+    SQLAlchemyAccessTokenDatabase,
+    SQLAlchemyBaseAccessTokenTable,
+)
+from sqlalchemy import ForeignKey, Integer
+from sqlalchemy.orm import mapped_column, Mapped
+
+from core.types.user_id import UserIdType
+from .base import Base
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
+
+class AccessToken(Base, SQLAlchemyBaseAccessTokenTable[UserIdType]):
+    user_id: Mapped[UserIdType] = mapped_column(
+        Integer, ForeignKey("users.id", on_delete="cascade"), nullable=False,
+    )
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyAccessTokenDatabase(session, cls)
